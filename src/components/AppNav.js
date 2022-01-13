e0c9e4f415f0e40fa0560622112467fce18f4f49
subhash_malireddy <ssm934@uowmail.edu.au>
2022-01-14 01:43:41 +1100

    adding logout functionality

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-
+import React, { useEffect } from 'react'
+import { Container, Nav, Button } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
+import { useNavigate } from 'react-router-dom';
+
+import * as Realm from "realm-web";
+
 import '../styles/general.scss'
 
 function AppNav() {
+    const app = new Realm.App({ id: "simple-task-reminder-zwzct" })
+    let navigate = useNavigate()
+    const logout = async () => {
+        await app.currentUser.logOut()
+        navigate('/')
+    }
+    // useEffect(() => {
+    //     console.log(app.currentUser ? true : false)
+    // })
     return (
         <div>
             <Navbar bg="dark" className="nav-brand">
                 <Container>
                     <Navbar.Brand>Simple Task Remainder</Navbar.Brand>
                 </Container>
+                {
+                    app.currentUser &&
+                    <Nav style={{ marginRight: "1rem" }}>
+                        <Button variant="danger" onClick={logout}>Logout</Button>
+                    </Nav>
+                }
             </Navbar>
         </div>
     )
